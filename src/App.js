b8eba9a9dbd748f7a5be3a3d7d1b9cea2bd49fea
lukasmyth96 <luka@nate.tech>
2020-07-18 13:03:14 +0100

    fixed protected routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
@@ -8,17 +8,31 @@ import LogIn from "./Pages/LogIn/LogIn";
 import SignUp from "./Pages/SignUp/SignUp";
 import Profile from "./Pages/Profile/Profile";
 
-function App() {
-  return (
-    <div>
-      <Router>
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/login" component={LogIn} />
-        <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/profile" component={Profile} />
-      </Router>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    authenticated: localStorage.getItem("token") !== null,
+  };
+
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/login" component={LogIn} />
+          <Route exact path="/signup" component={SignUp} />
+
+          {this.state.authenticated ?
+            <Route exact path="/profile" component={Profile} />
+            : <Redirect to="/login"/>}
+
+        </Router>
+      </div>
+    );
+  }
+
 }
 
 export default App;
