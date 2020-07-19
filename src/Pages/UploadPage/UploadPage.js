1f3abef095f3927dfa0a613e2d3b1ba0220da6e9
David-Ovidiu <naeovidiu@yahoo.com>
2020-07-19 16:41:26 +0300

    Image upload interface works

--- src/Pages/UploadPage/UploadPage.js
+++ src/Pages/UploadPage/UploadPage.js
@@ -6,6 +6,7 @@ import styles from "./UploadPage.module.css";
 class UploadPage extends Component {
   state = {
     title: "",
+    image1: undefined,
   };
 
   handle_change = (e) => {
@@ -18,18 +19,27 @@ class UploadPage extends Component {
     });
   };
 
-  handle_upload = (e) => {
+  handleFileUpload = (e) => {
+    const name = e.target.name;
+    const file = e.target.files[0];
+    this.setState((prevstate) => {
+      const newState = { ...prevstate };
+      newState[name] = file;
+      return newState;
+    });
+  };
+
+  handleSubmit = (e) => {
     e.preventDefault();
-    const data = {
-      title: this.state.title,
-    };
+    const data = new FormData();
+    data.append("title", this.state.title);
+    data.append("image1", this.state.image1);
     axios
-      .post(
-          "/products/", 
-          data, 
-          { headers: { 
-              Authorization: `JWT ${localStorage.getItem("token")}` 
-            },
+      .post("/products/", data, {
+        headers: {
+          Authorization: `JWT ${localStorage.getItem("token")}`,
+          "content-type": "multipart/form-data",
+        },
       })
       .then((response) => {
         localStorage.setItem("token", response.data.token);
@@ -44,7 +54,7 @@ class UploadPage extends Component {
     return (
       <div className={styles.UploadPageForm}>
         <h1>Upload your product</h1>
-        <form onSubmit={(e) => this.handle_upload(e)}>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
           <label htmlFor="title">Product title</label>
           <input
             type="text"
@@ -52,6 +62,7 @@ class UploadPage extends Component {
             value={this.state.title}
             onChange={this.handle_change}
           />
+          <input type="file" name="image1" onChange={this.handleFileUpload} />
           <input type="submit" />
         </form>
       </div>
