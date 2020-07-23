ba46c71102e4fc43b941351ef3fec77cc03b22e0
David-Ovidiu <naeovidiu@yahoo.com>
2020-07-23 18:23:25 +0300

    90% finished Our Mission page and created the layout for the Why Swap page

--- src/Pages/OurMission/OurMission.js
+++ src/Pages/OurMission/OurMission.js
@@ -8,6 +8,11 @@ import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
 import Container from "react-bootstrap/Container";
 
+import Modal from "react-bootstrap/Modal";
+import ModalBody from "react-bootstrap/ModalBody";
+import Button from "react-bootstrap/Button";
+
+import Plant from "../../img/green-leaf-plant-on-pot-824572.png";
 import Jake from "../../img/Jake.png";
 import Joe from "../../img/Joe.png";
 import Laura from "../../img/Laura.png";
@@ -16,84 +21,153 @@ import David from "../../img/David.png";
 class OurMission extends Component {
   render() {
     return (
-      <div className={styles.upperContainer}>
-        <MenuBar />
+      <div>
+        <div className={styles.upperContainer}>
+          <MenuBar />
+        </div>
+
         <div className={styles.textBox}>
           <div className={styles.wardrobeLife}>
             Give your wardrobe life, say hello to the swapping revolution.
           </div>
         </div>
+        <div className={styles.plantBox}>
+          <Container>
+            <div className="row align-items-center justify-content-between">
+              <div className="col-7">
+                <div className={styles.socialClothesText}>
+                  Swapdrop is a social clothes swapping platform, reinventing
+                  the way we consume fashion.
+                  <br />
+                  <br />
+                  Individuals, fashion, change. In that order.
+                </div>
+              </div>
+              <div className="col-2">
+                <div className={styles.plantPictureBox}>
+                  <img src={Plant} alt={"picture of plant"} />
+                </div>
+              </div>
+            </div>
+          </Container>
+        </div>
         <Container>
           <div className={styles.ourmissionBox}>
-            <span className={styles.ourmissionTitle}>Our mission.</span>
-            <br />
-            <br />
-            <span className={styles.ourmissionBody1}>
-              Swapdrop is a social clothes-swapping platform made by
-              individuals, for individuals. We wanted to create a site that was
-              built for individuals to take control of their fashion in a way
-              that does not exploit each other or our environment.
-              <br />
-              <br />
+            <span className={styles.ourmissionTitle}>
+              <div>Our mission.</div>
             </span>
-            <span className={styles.ourmissionBody2}>
-              We believe that fashion is the ultimate expression of an
-              individual. We grew tired with resale sites being for sellers and
-              not for individuals. We set about changing this - Sticking to
-              three key principles:
+            <br />
+            <span className={styles.ourmissionBody}>
+              <div className="">
+                Swapdrop is a social clothes-swapping platform made by
+                individuals, for individuals. We wanted to create a site that
+                was built for individuals to take control of their fashion in a
+                way that does not exploit each other or our environment.
+              </div>
+              <br />
+              <br />
+              <div>
+                We believe that fashion is the ultimate expression of an
+                individual. We grew tired with resale sites being for sellers
+                and not for individuals. We set about changing this - Sticking
+                to three key principles:
+              </div>
             </span>
           </div>
         </Container>
 
-        <div className={styles.teamBox}>
-          <Container>
-            <h1 className={styles.ourTeam}>Our team</h1>
-            <br />
-            <br />
-            <CardDeck>
-              <Card>
-                <Card.Img variant="top" src={Jake} className={styles.img} />
-                <Card.Body>
-                  <Card.Title className="text-center">
-                    <span className={styles.teamNames}>Jake Smyth</span>
-                  </Card.Title>
-                  <Card.Text className="text-center">Founder/CEO</Card.Text>
-                </Card.Body>
-              </Card>
-              <Card>
-                <Card.Img variant="top" src={Joe} className={styles.img} />
-                <Card.Body>
-                  <Card.Title className="text-center">
-                    <span className={styles.teamNames}>Joe Game</span>
-                  </Card.Title>
-                  <Card.Text className="text-center">Head of Design </Card.Text>
-                </Card.Body>
-              </Card>
-              <Card>
-                <Card.Img variant="top" src={Laura} className={styles.img} />
-                <Card.Body>
-                  <Card.Title className="text-center">
-                    <span className={styles.teamNames}>Laura Kane</span>
-                  </Card.Title>
-                  <Card.Text className="text-center">
-                    Head of Marketing
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-              <Card>
-                <Card.Img variant="top" src={David} className={styles.img} />
-                <Card.Body>
-                  <Card.Title className="text-center">
-                    <span className={styles.teamNames}>David-Ovidiu Nae</span>
-                  </Card.Title>
-                  <Card.Text className="text-center">
-                    Software Engineer
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </CardDeck>
-          </Container>
+        <div className={styles.middleArtboard}>
+          <div className={styles.teamBox}>
+            <Container>
+              <h1 className={styles.ourTeam}>Our team</h1>
+              <br />
+              <br />
+              <CardDeck>
+                <Card className="shadow p-3 mb-5 bg-white rounded">
+                  <Card.Img variant="top" src={Jake} className={styles.img} />
+                  <Card.Body>
+                    <Card.Title className="text-center">
+                      <span className={styles.teamNames}>Jake Smyth</span>
+                    </Card.Title>
+                    <Card.Text className="text-center">Founder/CEO</Card.Text>
+                  </Card.Body>
+                </Card>
+                <Card className="shadow p-3 mb-5 bg-white rounded">
+                  <Card.Img variant="top" src={Joe} className={styles.img} />
+                  <Card.Body>
+                    <Card.Title className="text-center">
+                      <span className={styles.teamNames}>Joe Game</span>
+                    </Card.Title>
+                    <Card.Text className="text-center">
+                      Head of Design{" "}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+                <Card className="shadow p-3 mb-5 bg-white rounded">
+                  <Card.Img variant="top" src={Laura} className={styles.img} />
+                  <Card.Body>
+                    <Card.Title className="text-center">
+                      <span className={styles.teamNames}>Laura Kane</span>
+                    </Card.Title>
+                    <Card.Text className="text-center">
+                      Head of Marketing
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+                <Card className="shadow p-3 mb-5 bg-white rounded">
+                  <Card.Img variant="top" src={David} className={styles.img} />
+                  <Card.Body>
+                    <Card.Title className="text-center">
+                      <span className={styles.teamNames}>David-Ovidiu Nae</span>
+                    </Card.Title>
+                    <Card.Text className="text-center">
+                      Software Engineer
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </CardDeck>
+            </Container>
+          </div>
         </div>
+
+        <Container>
+          <div className={styles.ourStoryBox}>
+            <span className={styles.ourStoryTitle}>
+              <div>Our story, so far.</div>
+            </span>
+            <br />
+            <span className={styles.ourStoryBody}>
+              <div className="">
+                Swapdrop is a Cardiff and London based early-stage startup,
+                having recently graduated from a 12 week startup incubator.
+              </div>
+              <br />
+              <br />
+              <div>
+                We’re attempting to change the way we consume fashion. We’re
+                currently testing our product before a national launch scheduled
+                for August.
+              </div>
+            </span>
+          </div>
+        </Container>
+        <div className={styles.modalBox}>
+          <Modal.Dialog className="shadow p-3 mb-5 bg-white rounded" size="xl">
+            <Modal.Body>
+              <p className={styles.modalBodyText}>Interested?</p>
+            </Modal.Body>
+            <Modal.Footer className={styles.modalFooter}>
+              <p className={styles.modalFooterText}>
+                Find out more about the work we’re doing at Swapdrop
+              </p>
+              <Button className={styles.emailUs} variant="danger">
+                {" "}
+                Email us
+              </Button>
+            </Modal.Footer>
+          </Modal.Dialog>
+        </div>
+        <div className={styles.bottomColor}></div>
       </div>
     );
   }
--- src/Pages/WhySwap/WhySwap.js
+++ src/Pages/WhySwap/WhySwap.js
@@ -4,6 +4,10 @@ import { withRouter } from "react-router";
 import styles from "./WhySwap.module.css";
 import MenuBar from "../../Components/MenuBar/MenuBar";
 
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
+import Container from "react-bootstrap/Container";
+
 class WhySwap extends Component {
   render() {
     return (
@@ -12,6 +16,55 @@ class WhySwap extends Component {
         <div className={styles.textBox}>
           <span className={styles.centerText}>Why Swap?</span>
         </div>
+        <Container className={styles.cardBox}>
+          <CardDeck>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Save money</Card.Title>
+                <Card.Text className={styles.cardText}>
+                  A swap with Swapdrop costs £4.99 inclusive of delivery.
+                  <br />
+                  <br />
+                  Swapping allows you to make use of your existing clothes which
+                  would otherwise go unused.
+                  <br />
+                  <br />
+                  Swapping is the most cost friendly way to renew your wardrobe.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Save money</Card.Title>
+                <Card.Text className={styles.cardText}>
+                  Our Earth can no longer keep up with our endless fast fashion
+                  consumption.
+                  <br />
+                  <br />
+                  True fashion is expression. At Swapdrop we believe in
+                  expressing our love and commitment to a better future for our
+                  planet.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Save money</Card.Title>
+                <Card.Text className={styles.cardText}>
+                  At Swapdrop we believe that fashion should be easy.We believe
+                  that renewing your wardrobe should be quick and stress free.
+                  <br />
+                  <br />
+                  That’s why it take 23 seconds to sign up to Swapdrop, 2 and a
+                  half minutes to upload an item.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </CardDeck>
+        </Container>
       </div>
     );
   }
