d55ac0a41937088a35ab6d0f81ab336504a9e65d
lukasmyth96 <luka@nate.tech>
2020-07-24 16:29:33 +0100

    tidy up

--- src/Pages/UploadPage/imageUtils.js
+++ src/Pages/UploadPage/imageUtils.js
@@ -1,8 +1,7 @@
 export function getCroppedImage(image, crop, fileName, extension) {
-  // crop is relative (0-100)
+  // crop object contains crop coordinates as percentage of whole image
  
     const canvas = document.createElement('canvas');
-
     const width = image.naturalWidth * crop.width / 100;
     const height = image.naturalHeight * crop.height / 100;
     const x = image.naturalWidth * crop.x / 100;
@@ -24,17 +23,16 @@ export function getCroppedImage(image, crop, fileName, extension) {
       height
     );
   
-    // As a blob
+
     return new Promise((resolve, reject) => {
       canvas.toBlob(blob => {
         const file = new File([blob], fileName)
         resolve(file);
-      }, `image/`);
+      }, `image/${extension}`);
     });
   }
 
 
-// Extract an Base64 Image's File Extension
 export function extractImageFileExtensionFromBase64 (base64Data) {
   return base64Data.substring('data:image/'.length, base64Data.indexOf(';base64'))
 }
